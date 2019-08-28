53d97613b16e7e231d10ba77a6ae06551a6736a8
droll-accelerator-1483 <srsani@gmail.com>
2019-08-28 14:50:32 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,7 @@
 class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -8,6 +9,10 @@ class Book
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
+  end
+  def genre=(genre)
+    @genre = genre
+    GENRES.append(genre)
   end
 
 end
