1ad671ed8d0d530f8449e2acddf50ace7c5e043f
droll-accelerator-1483 <srsani@gmail.com>
2019-08-28 14:25:32 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,8 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
+  BRANDS = []
+  
   def initialize(brand)
     @brand = brand
   end
@@ -9,6 +10,11 @@ class Shoe
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
+  end
+  
+ def brand=(brand)
+    @brand = brand
+    BRANDS.append(:brand)
   end
 
 end
